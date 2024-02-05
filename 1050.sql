5f6345197a16d4446a05be7dc20a8a184bdb5289
Likhon <likh.deshi@gmail.com>
2024-02-05 11:35:13 +0600

    1050. Actors and Directors Who Cooperated At Least Three Times

---
+++ 1050.sql
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times
+# Run on: MySQL
+SELECT
+  actor_id,
+  director_id
+FROM
+  ActorDirector
+GROUP BY
+  actor_id,
+  director_id
+HAVING
+  COUNT(*) >= 3
