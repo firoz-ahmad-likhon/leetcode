60bfd9ee3e8020e9b12fd59c906bee70d24a5986
Likhon <likh.deshi@gmail.com>
2024-02-07 16:28:05 +0600

    1890. The Latest Login in 2020

---
+++ 1890.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/the-latest-login-in-2020
+# Run on: MS SQL Server
+SELECT
+  user_id,
+  MAX(time_stamp) last_stamp
+FROM
+  Logins
+WHERE
+  YEAR(time_stamp) = 2020
+GROUP BY
+  user_id
